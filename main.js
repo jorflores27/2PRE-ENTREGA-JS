05c6a4862df16a730e5bd94563a511fda008ddb8
flores jorge <jor.ricardo.flores@gmail.com>
2024-10-02 23:12:02 -0300

    incorporando git

---
+++ main.js
@@ -0,0 +1,20 @@
+let nombre = prompt("Ingresa tu nombre");
+let edad = Number(prompt("Ingresa tu edad"));
+let foto = prompt("Profesión o Hobby");
+
+console.log(nombre);
+console.log(edad);
+console.log(foto);
+
+alert("Bienvenido " + nombre + "  a esta experiencia fotografica");
+
+const personal = {
+    nombre:"Jorge",
+    edad:"35",
+    foto:"hobby",
+}
+console.log(personal.nombre)
+console.log(personal.edad)
+console.log(personal.foto)
+
+
